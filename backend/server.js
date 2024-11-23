f493bca7c45694bfe81993fda3287d8c241654de
FarrukhS <909tjk@gmail.com>
2024-11-23 13:38:22 -0600

    built the api endpoints, model, routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000; // the or 5000 is hard-coded incase if the port is undefined in the .env file than we say go ahead and use the or statement.
 
-app.get("/products", (req, res) => {})
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.listen(5000, () => {
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
 
