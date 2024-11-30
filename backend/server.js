65ecdb1920dcdb068088f9fe2236b7f735d28b07
FarrukhS <909tjk@gmail.com>
2024-11-30 08:41:07 -0600

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -8,9 +9,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000; // the or 5000 is hard-coded incase if the port is undefined in the .env file than we say go ahead and use the or statement.
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
